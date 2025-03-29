0f46565f229c67358c055c61959847aec2420172
Gabriel Pillegand <gabriel.pillegand@epita.fr>
2025-03-29 19:37:54 +0100

    ajout d'un boutton pour retourner en haut de la page.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Hello World")
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0, // Position en haut de la page
+        behavior: 'smooth' // Défilement fluide
+    });
+}
